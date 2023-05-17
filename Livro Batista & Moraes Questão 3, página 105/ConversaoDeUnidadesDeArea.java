5d3460b4778fa02f5de8ece2acfb9ee7962befe7
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2023-05-17 08:02:10 -0300

    Create ConversaoDeUnidadesDeArea.java

---
+++ Livro Batista & Moraes Questão 3, página 105/ConversaoDeUnidadesDeArea.java
@@ -0,0 +1,17 @@
+public class ConversaoDeUnidadesDeArea {
+    public static void metrosQuadradosParaPesQuadrados(double metrosQuadrados) {
+        System.out.println(metrosQuadrados * 10.76);
+    }
+
+    public static void pesQuadradosParaCentimetrosQuadrados(double pesQuadrados) {
+        System.out.println(pesQuadrados * 929);
+    }
+
+    public static void milhasQuadradasParaAcres(double milhasQuadradas) {
+        System.out.println(milhasQuadradas * 640);
+    }
+
+    public static void acresParaPesQuadrados(double acres) {
+        System.out.println(acres * 43560);
+    }
+}
